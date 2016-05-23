f9594095bab836cc0be671aa4dd0d185c0a55fe4
duverssa <michellvaldivia1@live.com>
2016-05-23 16:29:26 -0700

    Added registration/login/logout features

--- languageapp/urls.py
+++ languageapp/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
 	url(r'^lesson01_Vocab/$', views.lesson01_Vocab, name='lesson01_Vocab'),
 	url(r'^lesson01_Dialogue/$', views.lesson01_Dialogue, name='lesson01_Dialogue'),
 	url(r'^lesson02_Vocab/$', views.lesson02_Vocab, name='lesson02_Vocab'),
+	url(r'^login/$', views.user_login, name='login'), #added login view 4/20
+    url(r'^logout/$', views.user_logout, name='logout'), # added logout view 4/20
+    url(r'^restricted/', views.restricted, name='restricted'),
+    url(r'^/register/$', views.register, name='register'), #added new URL for registration 4/13
 ]
--- languageapp/views.py
+++ languageapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import UserForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, HttpResponse
 
 # Create your views here.
 def home(request):
@@ -18,3 +22,54 @@ def lesson01_Dialogue(request):
 def lesson02_Vocab(request):
 	scriptLoc = "/static/javascript/lesson02_vocab.js"
 	return render(request, 'languageapp/lessonStruc.html', {'scriptLoc' : scriptLoc})
+
+def register(request):
+	registered = False
+
+	if request.method == 'POST':
+		user_form = UserForm(data=request.POST)
+		
+		if user_form.is_valid() :
+			user = user_form.save()
+
+			user.setpassword(user.password)
+			user.save()
+
+			registered = True
+		else:
+			print(user_form.errors)
+
+	else:
+		user_form = UserForm()
+
+	return render(request, 'languageapp/register.html', {'user_form': user_form,'registered': registered})
+
+def user_login(request):
+	if request.method == 'POST':
+
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+		user = authenticate(username=username, password=password)
+
+		if user:
+			if user.is_active:
+				login(request, user)
+				return hHttpResponsedRedirect('/')
+
+		else:
+			print("Invalid login details: {0}, {1}".format(username, password))
+			return HttpResponse("Invalid login details supplied")
+
+	else:
+		return render(request, 'languageapp/login.html')
+		
+@login_required
+def restricted(request):
+	return HttpResponse("Since you're logged in, you can see this text!")
+
+def user_logout(request):
+	logout(request)
+	return hHttpResponsedRedirect('/')
+
+
