847ef24770444ad2f2c34f5b4ca8119007b90500
mvaldivia99 <michellvaldivia1@live.com>
2023-05-24 16:44:06 -0700

    UPDATE DJANGO: updated models on_delete line 6

--- languageapp/models.py
+++ languageapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 #Create User
 class UserProfile(models.Model):
-	user = models.OneToOneField(User)
+	user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
 
 	def __unicode__(self):
 		return self.user.username
