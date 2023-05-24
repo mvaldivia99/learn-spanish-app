856c8f47250260fa864533e80fef17ce132020bc
mvaldivia99 <michellvaldivia1@live.com>
2023-05-24 16:41:34 -0700

    UPDATE DJANGO: changed urls to regex path

--- languageapp/urls.py
+++ languageapp/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import url
+from django.urls import include, re_path
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.home, name='home'),
-	url(r'^lessons/$', views.lessons, name='lessons'),
-	url(r'^lesson01_Vocab/$', views.lesson01_Vocab, name='lesson01_Vocab'),
-	url(r'^lesson01_Dialogue/$', views.lesson01_Dialogue, name='lesson01_Dialogue'),
-	url(r'^lesson02_Vocab/$', views.lesson02_Vocab, name='lesson02_Vocab'),
-	url(r'^login/$', views.user_login, name='login'), 
-    url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^restricted/', views.restricted, name='restricted'),
-    url(r'^register/$', views.register, name='register'), 
+	re_path(r'^$', views.home, name='home'),
+	re_path(r'^lessons/$', views.lessons, name='lessons'),
+	re_path(r'^lesson01_Vocab/$', views.lesson01_Vocab, name='lesson01_Vocab'),
+	re_path(r'^lesson01_Dialogue/$', views.lesson01_Dialogue, name='lesson01_Dialogue'),
+	re_path(r'^lesson02_Vocab/$', views.lesson02_Vocab, name='lesson02_Vocab'),
+	re_path(r'^login/$', views.user_login, name='login'), 
+    re_path(r'^logout/$', views.user_logout, name='logout'),
+    re_path(r'^restricted/', views.restricted, name='restricted'),
+    re_path(r'^register/$', views.register, name='register'), 
 ]
